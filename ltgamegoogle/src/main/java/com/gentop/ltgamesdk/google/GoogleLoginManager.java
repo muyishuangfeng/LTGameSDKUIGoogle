1c873db61b6e54415adac9e812a17be2b9ba8c6d
silence <yangkemuyi@sina.com>
2019-11-27 10:47:40 +0800

    first commit

---
+++ ltgamegoogle/src/main/java/com/gentop/ltgamesdk/google/GoogleLoginManager.java
@@ -0,0 +1,92 @@
+package com.gentop.ltgamesdk.google;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import com.gentop.ltsdk.common.impl.OnLoginSuccessListener;
+import com.gentop.ltsdk.common.manager.LoginResultManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class GoogleLoginManager {
+
+    private static final String TAG = GoogleLoginManager.class.getSimpleName();
+
+    public static void initGoogle(Activity context, String clientID, int selfRequestCode) {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(clientID)
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(context, gso);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        context.startActivityForResult(signInIntent, selfRequestCode);
+    }
+
+
+    public static void onActivityResult(String url, int requestCode, Intent data, int selfRequestCode,
+                                        Context context, String LTAppID, String LTAppKey, String adID,
+                                        String packageID,
+                                        OnLoginSuccessListener mListener) {
+        if (requestCode == selfRequestCode) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            if (!TextUtils.isEmpty(adID)) {
+                handleSignInResult(context, url, LTAppID, LTAppKey, adID, packageID, task, mListener);
+            }
+        }
+    }
+
+
+    private static void handleSignInResult(Context context, String url, String LTAppID,
+                                           String LTAppKey, String adID, String packageID,
+                                           @NonNull Task<GoogleSignInAccount> completedTask,
+                                           OnLoginSuccessListener mListener) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String idToken = account.getIdToken();
+            Map<String, Object> map = new WeakHashMap<>();
+            if (!TextUtils.isEmpty(idToken)) {
+                if (!TextUtils.isEmpty(adID)) {
+                    map.put("access_token", idToken);
+                    map.put("platform", 2);
+                    map.put("adid", "");
+                    map.put("gps_adid", adID);
+                    map.put("platform_id", packageID);
+                }
+            }
+            LoginResultManager.googleLogin(context, url, LTAppID,
+                    LTAppKey, map, mListener);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 退出登录
+     */
+    public static void GoogleSingOut(Context context, String clientID, final OnGoogleSignOutListener mListener) {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(clientID)
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(context, gso);
+        mGoogleSignInClient.signOut().addOnCompleteListener((Activity) context, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                mListener.onSignOutSuccess();
+            }
+        });
+    }
+
+
+}
---
+++ ltgamegoogle/src/main/java/com/gentop/ltgamesdk/google/OnGoogleSignOutListener.java
@@ -0,0 +1,10 @@
+package com.gentop.ltgamesdk.google;
+
+/**
+ * 退出登录
+ */
+public interface OnGoogleSignOutListener {
+
+    void onSignOutSuccess();
+
+}
